d6e68828c98045c08f19378f3eff3af19bc5afe8
SultanMehmood666 <SultanMehmood@@123>
2025-01-05 19:05:59 +0500

    first commit

---
+++ Beckend/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const app = require('./src/app.js');
+const port = 5000;
+const server = http.createServer(app);
+server.listen(port, ()=>{console.log(`Server is running on ${port}`)})
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+    }) //attaching socket.io with server here
+
+
+// Everytime the user connect
+// socket is an argument containing details of connected user
+ io.on('connection', (socket)=>{
+    console.log(`A new User has been Connected: ${socket.id}`);
+ })
